b00ab931c2cf7f50180a0d2410b4c258dadf773f
João Faria <jfaria.gmr@gmail.com>
2016-10-24 19:56:44 +0100

    mongo Schemas

--- routes/api.js
+++ routes/api.js
@@ -1,20 +1,52 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var router = express.Router();
-var Schema = mongoose.Schema;
-
-var InscSchema = new Schema({
-  bi: String,
-  nome: String, 
-  morada: String,
-  nascimento: String
-});
-
-var Inscricao = mongoose.model('inscricao', InscSchema);
+var mongoose = require('mongoose');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/users/:ValHash', function(req, res, next) {
+  res.status(200).send('Inscrição com sucesso');
+});
+
+var userSchema = mongoose.Schema({
+    username: String,
+    password: String,
+    nome: String,
+    ratingsPraias: {
+      cidade: String,
+      localidade: String,
+      praia: String,
+      ratingGeral: Number,
+      ratingCrianças: Number,
+      ratingSeguranca: Number,
+      ratingEquipamento: Number
+    },
+    credito: Number,
+    timeStamp: { type: Date, default: Date.now }
+});
+
+var ratingsSchema = mongoose.Schema({
+  cidade: String,
+  localidade: String,
+  tempo: [{
+    data: { type: Date, default: Date.now },
+    classificação: Number,
+    ratingGeral: Number,
+    ratingGeralNum: Number,
+    ratingCriancas: Number,
+    ratingCriancasNum: Number,
+    ratingSeguranca: Number,
+    ratingSegurançaNum: Number,
+    ratingEquipamento: Number,
+    ratingEquipamentoNum: Number
+  }]
+});
+
+var user = mongoose.model('user', userSchema);
+var ratings = mongoose.model('ratings', ratingsSchema);
 
 module.exports = router;
