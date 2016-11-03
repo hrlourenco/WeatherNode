6e47ddb46fd3cb6750e17e0bf74be9f405e7829a
Hugo Lourenco <hugo.lourenco@outlook.com>
2016-11-03 00:56:14 +0000

    Actual Location

--- public/controllers.js
+++ public/controllers.js
@@ -7,10 +7,37 @@ angular.module('weatherIPCA')
     });
 
 angular.module('weatherIPCA')
-    .controller('homeController', function($scope, $location, $rootScope) {
+    .controller('homeController', function($scope, $location, $rootScope, $http) {
+        //topbar na ngView
         $rootScope.hideTopBar = false;
+
+        //método responsável pelo redirecionamento
         $scope.goView = function(view) {
             $location.path(view);
         }
+
+        //variavel onde é guardado o valor a apresentar na textbox principal
+        $scope.actualLocation = "Praia";
+
+        //método achamada quando é feito o load deste controller
+        $scope.$on('$routeChangeSuccess', function () {
+            $scope.actualLocationFunction();
+        });
+        
+        //função que retorna a localização actual
+        $scope.actualLocationFunction = function() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(function (position) {
+                    $http({
+                        method: 'GET',
+                        url: 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + position.coords.latitude + ',' + position.coords.longitude + '&key=AIzaSyAiJO35RmsBztphqjN2q6KidJskplx6fCw'
+                    }).then(function successCallback(response) {
+                        $scope.actualLocation = response.data.results[1];
+                    }, function errorCallback(response) {
+                        $scope.actualLocation = "Praia"
+                    });
+                });
+            }
+        };
     });
 
