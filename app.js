121264537867c0ab2824a2e954b501b76cdda8ee
Hugo Lourenco <hugo.lourenco@outlook.com>
2016-11-11 23:13:18 +0000

    CORS fix

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT');
   //res.header('Access-Control-Allow-Headers', 'POST, GET, OPTIONS, PUT');
+  next();
 });
 
 app.use('/api/v1', api);
