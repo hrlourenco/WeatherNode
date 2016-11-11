94422bc666b5e0244ff90f709321e1fc640e58ac
Hugo Lourenco <hugo.lourenco@outlook.com>
2016-11-11 23:16:58 +0000

    coors headers

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'node_modules')));
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT');
-  //res.header('Access-Control-Allow-Headers', 'POST, GET, OPTIONS, PUT');
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   next();
 });
 
